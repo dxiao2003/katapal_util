3ac033518014776c7b928ff66f412f5dc6a03c8a
David Xiao <dxiao@katapal.com>
2016-08-12 14:29:39 -0400

    updated version and requires

--- katapal_util/__init__.py
+++ katapal_util/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'katapal_util'
-__version__ = '0.1'
+__version__ = '0.2'
 __author__ = 'David Xiao'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Katapal, Inc.'
